7b422b00993a48c6c98c13b4149c946154902fd4
Nazwazhraa <nazwaazzahra028@gmail.com>
2022-08-23 13:52:56 +0700

    variabel aritmatika

---
+++ latihan/latihanstrng.js
@@ -0,0 +1,9 @@
+var word = "javascript";
+var seccond = 'is';
+var third = 'awesome';
+var fourth = 'and';
+var fifth = '1';
+var sixth = 'love';
+var seventh = 'it';
+
+console.log(word + " " + seccond + " " + third + " " + fourth + " " + fifth + " " + sixth + " " + seventh);
